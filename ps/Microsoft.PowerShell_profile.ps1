d71510e30ccd826f793a32f5107b034019dac87a
Andy Sweetman <AndySw@users.noreply.github.com>
2017-08-03 14:40:50 +0100

    Update Microsoft.PowerShell_profile.ps1

--- ps/Microsoft.PowerShell_profile.ps1
+++ ps/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,30 @@
 sl D:\Users\andys\Projects
 Import-Module D:\Users\andys\Documents\WindowsPowerShell\Modules\posh-git\0.7.1\posh-git.psd1
 
-function sudo  
+function Start-PreviousCommandAsAdministrator
 {
-    $file, [string]$arguments = $args;
-    $psi = new-object System.Diagnostics.ProcessStartInfo $file;
-    $psi.Arguments = $arguments;
-    $psi.Verb = "runas";
-    $psi.WorkingDirectory = get-location;
-    [System.Diagnostics.Process]::Start($psi);
+    $cmd = (Get-History ((Get-History).Count))[0].CommandLine
+    Write-Host "Running $cmd in $PWD"
+    sudo powershell -NoExit -Command "pushd '$PWD'; Write-host 'cmd to run: $cmd'; $cmd"
 }
 
+function sudo  
+{
+	if($args[0] -eq '!!')
+	{
+		Start-PreviousCommandAsAdministrator;
+	}
+	else
+	{
+
+	    $file, [string]$arguments = $args;
+	    $psi = new-object System.Diagnostics.ProcessStartInfo $file;
+	    $psi.Arguments = $arguments;
+	    $psi.Verb = "runas";
+	    $psi.WorkingDirectory = get-location;
+	    [System.Diagnostics.Process]::Start($psi);
+	}
+}
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
