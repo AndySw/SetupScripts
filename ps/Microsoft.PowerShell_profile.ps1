2411702ab724cb876611030a61e1c41df494da7b
Andy Sweetman <adsweetman@gmail.com>
2017-08-03 14:15:46 +0100

    Added powershell profile

---
+++ ps/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+sl D:\Users\andys\Projects
+Import-Module D:\Users\andys\Documents\WindowsPowerShell\Modules\posh-git\0.7.1\posh-git.psd1
+
+function sudo  
+{
+    $file, [string]$arguments = $args;
+    $psi = new-object System.Diagnostics.ProcessStartInfo $file;
+    $psi.Arguments = $arguments;
+    $psi.Verb = "runas";
+    $psi.WorkingDirectory = get-location;
+    [System.Diagnostics.Process]::Start($psi);
+}
+
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
