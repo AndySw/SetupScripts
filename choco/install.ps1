c249cd061c926d23b2932e650ac0d3b490f3871e
Andy Sweetman <AndySw@users.noreply.github.com>
2017-08-03 16:14:27 +0100

    Update install.ps1

--- choco/install.ps1
+++ choco/install.ps1
@@ -1,9 +1,8 @@
 # Chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# install applications
-cinst virtualclonedrive
-cinst sysinternals
-cinst msysgit
-cinst fiddler
+# install applications
+cinst fiddler
 cinst tortoisesvn
+cinst micro
+cinst 7zip
