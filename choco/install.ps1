d05f743ac0cf390de680341b9b6faf8dbc574a83
Andy Sweetman <adsweetman@gmail.com>
2017-08-03 16:04:26 +0100

    choco packages

---
+++ choco/install.ps1
@@ -0,0 +1,9 @@
+# Chocolatey
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# install applications
+cinst virtualclonedrive
+cinst sysinternals
+cinst msysgit
+cinst fiddler
+cinst tortoisesvn
